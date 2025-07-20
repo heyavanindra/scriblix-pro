fe583b217d96e5ec37b4e1b70036fbff3e193a68
Avanindra <heyavanindra@gmail.com>
2025-07-20 13:20:39 +0530

    added lil animation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,15 +2,27 @@ import Container from "../components/container";
 import Footer from "../components/footer";
 import LatestBlogs from "../components/latest-blogs";
 import SubscribeCard from "../components/subscribe-card";
+import { motion } from "motion/react";
 
 const Home = () => {
   return (
     <Container className="">
       <div className="relative flex  font-style flex-col items-center   min-h-screen bg-main ">
         <div className="mt-40 max-lg:mt-30 text-center tracking-wider">
-          <h1 className="text-9xl max-lg:text-5xl max-xl:text-7xl text-primary-text font-bold  font-body mb-4 ">
+          <motion.h1
+            className="text-9xl max-lg:text-5xl max-xl:text-7xl text-primary-text font-bold  font-body mb-4 "
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5,
+              ease: "easeInOut"
+             }}
+          >
             Read under 5 minutes.
-          </h1>
+          </motion.h1>
         </div>
         <div className="absolute top-[30%] max-lg:top-[20%] left-1/2 -translate-x-1/2 -z-0 w-full overflow-hidden">
           <img
